dc19a13c9f0464bca27e438f4a05ce0e9bf718bd
Brandon Lu <123blu@frhsd.com>
2023-01-11 08:51:02 -0500

    worked on rotating image and also worked on the placeholder for the html

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,15 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-  
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(/* function */ callback, /* DOMElement */ element){
+			  window.setTimeout(callback, 1000 / 60);
+			};
+  })();
+
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -18,25 +18,25 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
-	var currentTime = new Date().getTime();
-	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
-	}
+  requestAnimFrame( animate );
+  var currentTime = new Date().getTime();
+  if (mLastFrameTime === 0) {
+	  mLastFrameTime = currentTime;
+  }
 
-	if ((currentTime - mLastFrameTime) > mWaitTime) {
-		swapPhoto();
-		mLastFrameTime = currentTime;
-	}
+  if ((currentTime - mLastFrameTime) > mWaitTime) {
+	  swapPhoto();
+	  mLastFrameTime = currentTime;
+  }
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 function swapPhoto() 
 {
-	document.getElementById('photo').src = mImages[mCurrentIndex].img;
-	console.log('swap photo');
-	mLastFrameTime = 0;
-	mCurrentIndex += 1;
+  document.getElementById('photo').src = mImages[mCurrentIndex].img;
+  console.log('swap photo');
+  mLastFrameTime = 0;
+  mCurrentIndex += 1;
 }
 
 
@@ -55,57 +55,57 @@ var mUrl = 'images.json';
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
 mRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-		mJson = JSON.parse(mRequest.responseText);
-		console.log("before iteration");
-		iterateJSON(mJson);
-	}
-  };
-  mRequest.open("GET", mUrl, true);
-  mRequest.send();
+  if (this.readyState == 4 && this.status == 200) {
+	  mJson = JSON.parse(mRequest.responseText);
+	  console.log("before iteration");
+	  iterateJSON(mJson);
+  }
+};
+mRequest.open("GET", mUrl, true);
+mRequest.send();
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		mImages.push(galleryImage);
-	}
+  return function(e) {
+	  galleryImage.img = e.target;
+	  mImages.push(galleryImage);
+  }
 }
 
 $(document).ready( function() {
-	
-	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
-	
+  
+  // This initially hides the photos' metadata information
+  $('.details').eq(0).hide();
+  
 });
 
 window.addEventListener('load', function() {
-	
-	console.log('window loaded');
+  
+  console.log('window loaded');
 
 }, false);
 
 function GalleryImage() {
-	//implement me as an object to hold the following data about an image:
-	var location;
-	var description;
-	var date;
-	var img;
+  //implement me as an object to hold the following data about an image:
+  var location;
+  var description;
+  var date;
+  var img;
 }
 
 function iterateJSON(mJson)
 {
-	console.log("in iteration");
+  console.log("in iteration");
 for( x= 0; x < mJson.images.length; x++)
 {
-	mImages[x]= new GalleryImage();
-	mImages[x].location = mJson.images[x].imgLocation;
-	mImages[x].description = mJson.images[x].description;
-	mImages[x].img = mJson.images[x].imgPath;
-	mImages[x].date = mJson.images[x].date;
-	console.log("img path: " + mJson.images[x].imgPath);
-	console.log("img path: " + mImages[x].img);
+  mImages[x]= new GalleryImage();
+  mImages[x].location = mJson.images[x].imgLocation;
+  mImages[x].description = mJson.images[x].description;
+  mImages[x].img = mJson.images[x].imgPath;
+  mImages[x].date = mJson.images[x].date;
+  console.log("img path: " + mJson.images[x].imgPath);
+  console.log("img path: " + mImages[x].img);
 }
 }
